255c343824e0c1ffe6f038002741dad6b204a461
Corlius <corliuswang@gmail.com>
2024-03-23 20:47:04 +0800

    Request user input parameters

--- Appstorrent Language Switcher/Appstorrent-Language-Switcher.user.js
+++ Appstorrent Language Switcher/Appstorrent-Language-Switcher.user.js
@@ -2,7 +2,7 @@
 // @name            Appstorrent 翻译切换器
 // @name:en         Appstorrent Language Switcher
 // @namespace       https://greasyfork.org/zh-CN/scripts/490641
-// @version         1.1
+// @version         1.3
 // @description     为appstorrent.ru自动切换翻译语言
 // @description:en  Automatically switch translation language for appstorrent.ru
 // @author          Corlius
@@ -18,6 +18,26 @@
 
 (function() {
     'use strict';
+
+    function isFirstInstall() {
+        const isInstalled = GM_getValue('isInstalled', false);
+        if (!isInstalled) {
+            GM_setValue('isInstalled', true);
+            return true;
+        }
+        return false;
+    }
+
+    if (isFirstInstall()) {
+        const detailsPageLangInitial = prompt('请选择详情页面的默认语言:\nPlease select the default language for the details pages:\nr = ru 俄语\ne = en 英语\nd = de 德语\nz = zh-TW 中文', 'e');
+        const otherPageLangInitial = prompt('请选择其他页面的默认语言:\nPlease select the default language of other pages:\nr = ru 俄语\ne = en 英语\nd = de 德语\nz = zh-TW 中文', 'e');
+        const pollingInterval = prompt('请设置监听间隔(毫秒),默认为1500:\nPlease set the listening interval (milliseconds), the default is 1500.', '1500');
+    
+        GM_setValue('detailsPageLangInitial', detailsPageLangInitial);
+        GM_setValue('otherPageLangInitial', otherPageLangInitial); 
+        GM_setValue('pollingInterval', pollingInterval);
+        window.location.reload();
+    }
 
     // 根据语言代号改变网页语言的函数
     function changeLanguage(languageCode) {
@@ -44,6 +64,9 @@
     var detailsPageLangInitial = GM_getValue('detailsPageLangInitial', 'e');
     var otherPageLangInitial = GM_getValue('otherPageLangInitial', 'e');
 
+    // 获取用户设置的监听间隔
+    var pollingInterval = GM_getValue('pollingInterval', 1500);
+
     // 转换用户设置的语言首字母为完整的语言代码
     var detailsPageLang = getLanguage(detailsPageLangInitial);
     var otherPageLang = getLanguage(otherPageLangInitial);
@@ -51,19 +74,18 @@
     // 检查当前页面是否是详情页
     var isDetailsPage = window.location.pathname.endsWith(".html");
 
-    // 每1.5秒监听一次页面语言变化并更新
+    // 每pollingInterval毫秒监听一次页面语言变化并更新
     setInterval(function() {
         var currentLang = document.documentElement.getAttribute('lang');
         var targetLang = isDetailsPage ? detailsPageLang : otherPageLang;
-
         if (currentLang !== targetLang) {
             changeLanguage(targetLang);
         }
-    }, 1500);
+    }, pollingInterval);
 
     // 添加语言选择命令到菜单
     GM_registerMenuCommand('设置详情页语言 / Set details page language', function() {
-        var langInitial = prompt('r=ru俄语，e=en英语，d=de德语，z=zh-TW中文:', detailsPageLangInitial);
+        var langInitial = prompt('r = ru 俄语，e = en 英语，d= de 德语，z = zh-TW中文:', detailsPageLangInitial);
         if (langInitial) {
             GM_setValue('detailsPageLangInitial', langInitial);
             window.location.reload();
@@ -71,9 +93,18 @@
     });
 
     GM_registerMenuCommand('设置其他页面语言 / Setting other page languages', function() {
-        var langInitial = prompt('r=ru俄语，e=en英语，d=de德语，z=zh-TW中文:', otherPageLangInitial);
+        var langInitial = prompt('r = ru 俄语，e = en 英语，d= de 德语，z = zh-TW中文:', otherPageLangInitial);
         if (langInitial) {
             GM_setValue('otherPageLangInitial', langInitial);
+            window.location.reload();
+        }
+    });
+
+    // 添加设置监听间隔命令到菜单
+    GM_registerMenuCommand('设置监听间隔 / Set the listening interval', function() {
+        var interval = prompt('默认值为1500毫秒，如果遇到页面频繁刷新，请提高此参数：\nThe default value is 1500 ms, if you encounter frequent page refreshes, please increase this parameter:', pollingInterval);
+        if (interval !== null) {
+            GM_setValue('pollingInterval', interval);
             window.location.reload();
         }
     });
--- Appstorrent Language Switcher/README.md
+++ Appstorrent Language Switcher/README.md
@@ -13,9 +13,11 @@ This script automatically switches the translation language for the [appstorrent
 
 - 脚本添加了菜单命令，以分别设置详细页面和其他页面的首选语言。
 - 可以在脚本菜单中输入语言的首字母来设置语言（`r`= 俄语，`e`= 英语，`d`= 德语，`z`= 中文）。
+- 脚本的默认监听频率为1500毫秒，如果遇到页面频繁刷新，请提高此参数。
 - Appstorrent的内置翻译器只支持繁体中文，如需简体请配合 https://greasyfork.org/zh-CN/scripts/24300 食用。
 - 默认语言均为英文。
 - The script adds menu commands to set the preferred language for detail pages and other pages respectively.
 - You can enter the first letter of the language in the script menu to set the language (`r`= Russian, `e`= English, `d`= German, `z`= Chinese).
+- The default listening frequency of the script is 1500 milliseconds, if you encounter frequent page refreshes, please increase this parameter.
 - Appstorrent's built-in translator only supports traditional Chinese. If you need simplified Chinese, please use it with https://greasyfork.org/zh-CN/scripts/24300.
 - The default languages are all English.
